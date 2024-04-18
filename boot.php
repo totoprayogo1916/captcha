04c1db8bd5772206dd25be1cb548c1f584cac88c
Toto <totoprayogo1916@gmail.com>
2024-04-18 21:03:18 +0700

    Delete boot.php

--- boot.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-defined('DS') or exit('No direct script access.');
-
-Autoloader::map(['Esyede\Captcha' => __DIR__.DS.'libraries'.DS.'captcha.php']);
-
-System\Validator::register('captcha', function ($attribute, $value) {
-    return Esyede\Captcha::check($value);
-});
