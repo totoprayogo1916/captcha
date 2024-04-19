d89c7ddfa32f8319b81df7d6016d4e013d8e5f19
totoprayogo1916 <2387514+totoprayogo1916@users.noreply.github.com>
2024-04-19 08:58:55 +0700

    update

--- src/Captcha.php
+++ src/Captcha.php
@@ -2,10 +2,8 @@
 
 namespace Esoftdream\Captcha;
 
-use System\URL;
-use System\Str;
-use System\Hash;
-use System\Session;
+use Config\Services;
+use Exception;
 
 class Captcha
 {
@@ -16,6 +14,9 @@ class Captcha
 
     public static function make($case_sensitive = false)
     {
+        helper('string');
+        $encrypter = Services::encrypter();
+
         if (empty(static::$backgrounds)) {
             static::backgrounds();
         }
@@ -28,11 +29,14 @@ class Captcha
         static::$characters = str_replace(
             ['0', '1', '5', 'i', 'I', 'k', 'K', 'l', 'L', 'o', 'O', 's', 'S', 'w', 'W'],
             ['6', '4', '8', '2', '3', 'z', 'Z', 'p', 'P', 'h', 'H', 'x', 'X', 'v', 'V'],
-            Str::random(5)
+            random_string('alnum', 5)
         );
 
         $characters = static::$case_sensitive ? static::$characters : strtolower(static::$characters);
-        Session::put('captcha.hash', Hash::make($characters));
+        $hash = $encrypter->encrypt($characters);
+
+        // save to session
+        session()->set('captcha.hash', $hash);
 
         $bg = static::background();
         $font = static::font();
@@ -41,10 +45,16 @@ class Captcha
 
         switch ($info['mime']) {
             case 'image/jpg':
-            case 'image/jpeg': $old = imagecreatefromjpeg($bg); break;
-            case 'image/gif':  $old = imagecreatefromgif($bg);  break;
-            case 'image/png':  $old = imagecreatefrompng($bg);  break;
-            default:           throw new \Exception('Only JPG, PNG and GIF are supported for backgrounds.');
+            case 'image/jpeg': $old = imagecreatefromjpeg($bg);
+                break;
+
+            case 'image/gif':  $old = imagecreatefromgif($bg);
+                break;
+
+            case 'image/png':  $old = imagecreatefrompng($bg);
+                break;
+
+            default:           throw new Exception('Only JPG, PNG and GIF are supported for backgrounds.');
         }
 
         // default settings
@@ -59,23 +69,23 @@ class Captcha
         imagecopyresampled($new, $old, 0, 0, 0, 0, $width, $height, $info[0], $info[1]);
         imagedestroy($old);
 
-        $color = md5(Str::random(5));
+        $color = md5(random_string('alnum', 5));
 
         for ($i = 0; $i < 5; $i++) {
             $colors = [
-                hexdec(substr($color, rand(0, 31), 2)),
-                hexdec(substr($color, rand(0, 31), 2)),
-                hexdec(substr($color, rand(0, 31), 2)),
-                hexdec(substr($color, rand(0, 31), 2)),
-                hexdec(substr($color, rand(0, 31), 2)),
+                hexdec(substr($color, mt_rand(0, 31), 2)),
+                hexdec(substr($color, mt_rand(0, 31), 2)),
+                hexdec(substr($color, mt_rand(0, 31), 2)),
+                hexdec(substr($color, mt_rand(0, 31), 2)),
+                hexdec(substr($color, mt_rand(0, 31), 2)),
             ];
 
             $gap = 10 + ($i * $space);
-            $w = rand(-10, 15);
-            $h = rand($height - 10, $height - 5);
-            $fg = imagecolorallocate($new, $colors[rand(1, 3)], $colors[rand(1, 4)], $colors[rand(0, 4)]);
+            $w = mt_rand(-10, 15);
+            $h = mt_rand($height - 10, $height - 5);
+            $fg = imagecolorallocate($new, $colors[mt_rand(1, 3)], $colors[mt_rand(1, 4)], $colors[mt_rand(0, 4)]);
 
-            imagettftext($new, rand(18, 20), $w, $gap, $h, $fg, $font, static::$characters[$i]);
+            imagettftext($new, mt_rand(18, 20), $w, $gap, $h, $fg, $font, static::$characters[$i]);
         }
 
         header('Cache-Control: no-cache, no-store, max-age=0, must-revalidate');
@@ -89,42 +99,52 @@ class Captcha
     public static function check($value)
     {
         $value = trim((string) (static::$case_sensitive ? $value : strtolower($value)));
-        $hash = Session::get('captcha.hash', '');
-        return $value && $hash && Hash::check($value, $hash);
+        $hash = session()->get('captcha.hash');
+
+        return $value && $hash && static::hash_check($value, $hash);
+    }
+
+    public static function hash_check($value, $hash): bool
+    {
+        $encrypter = Services::encrypter();
+
+        $hash_ori = $encrypter->decrypt($hash);
+
+        return (bool) ($hash_ori === $value);
     }
 
     public static function url()
     {
-        return URL::to('captcha?'.mt_rand(1, 100000));
+        return base_url('captcha') . '?' . mt_rand(1, 100000);
     }
 
     protected static function fonts()
     {
-        $fonts = glob(path('assets').'packages'.DS.'captcha'.DS.'fonts'.DS.'*.ttf');
+        $fonts = glob('./assets/fonts/*.ttf');
         static::$fonts = (is_array($fonts) && ! empty($fonts)) ? $fonts : [];
     }
 
     protected static function backgrounds()
     {
-        $backgrounds = glob(path('assets').'packages'.DS.'captcha'.DS.'backgrounds'.DS.'*.png');
+        $backgrounds = glob('./assets/backgrounds*.png');
         static::$backgrounds = (is_array($backgrounds) && ! empty($backgrounds)) ? $backgrounds : [];
     }
 
     protected static function background()
     {
         if (empty(static::$backgrounds)) {
-            throw new \Exception('No backgrounds found to operate with.');
+            throw new Exception('No backgrounds found to operate with.');
         }
 
-        return static::$backgrounds[rand(0, count(static::$backgrounds) - 1)];
+        return static::$backgrounds[mt_rand(0, count(static::$backgrounds) - 1)];
     }
 
     protected static function font()
     {
         if (empty(static::$fonts)) {
-            throw new \Exception('No fonts found to operate with.');
+            throw new Exception('No fonts found to operate with.');
         }
 
-        return static::$fonts[rand(0, count(static::$fonts) - 1)];
+        return static::$fonts[mt_rand(0, count(static::$fonts) - 1)];
     }
 }
